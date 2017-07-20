b885ae747a9eb99b6dbd8015b54e6a76002dc033
Prova Django <prova@django.com>
2017-07-20 11:55:18 +0200

    Gestione del titolo diverso in caso di New o Edit del post

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
+    titolo="Nuovo"
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -24,9 +25,10 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form, 'titolo': titolo})
 
 def post_edit(request, pk):
+    titolo="Edit"
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -38,4 +40,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form, 'titolo': titolo})
